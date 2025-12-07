58d806d0847ec7027dedc4cb581e10f7be315c15
Adil Sakout <adil.sakout@gmail.com>
2025-12-07 16:30:00 +0100

    Add Home Widget Extension with Configuration and Control Features
    - Introduced a new Home Widget extension, including the main widget, configuration intent, and control widget for timer functionality.
    - Added necessary entitlements and Info.plist for widget integration.
    - Created asset catalogs for app icons and colors to enhance visual presentation.
    - Implemented a simple timeline provider and entry view for displaying widget content.

---
+++ ios/HomeWidget/HomeWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWidgetBundle.swift
+//  HomeWidget
+//
+//  Created by adil sakout on 7/12/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HomeWidget()
+        HomeWidgetControl()
+    }
+}
